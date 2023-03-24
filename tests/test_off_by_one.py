59fca4ce1dca9acdc7999a90e399127d422f912b
joncrall <erotemic@gmail.com>
2023-03-24 12:06:08 -0400

    Add off by one test

---
+++ tests/test_off_by_one.py
@@ -0,0 +1,74 @@
+
+def off_by_one_with_upscale():
+    import delayed_image
+    import numpy as np
+    delayed = delayed_image.DelayedLoad.demo(key='astro').prepare()
+
+    orig = delayed.finalize()
+
+    x = delayed.scale(2.0001)[0:4, 0:4]
+    data1 = x.finalize(optimize=False, nodata_method='float')
+    data2 = x.finalize(nodata_method='float')
+    assert np.all(data2 == data1)
+
+    if 0:
+        import kwplot
+        kwplot.autompl()
+        kwplot.plt.ion()
+        kwplot.imshow(data1, pnum=(1, 2, 1))
+        kwplot.imshow(data2, pnum=(1, 2, 2))
+
+
+def off_by_one_with_multi_scale():
+    import delayed_image
+    import numpy as np
+    delayed = delayed_image.DelayedLoad.demo(key='astro').prepare()
+    orig = delayed.finalize()
+
+    for s in np.linspace(0.4, 2.5, 100).tolist() + [0.5, 1.0, 2.0]:
+        x = delayed.scale(s)[0:4, 0:4]
+        data1 = x.finalize(optimize=False, nodata_method='float')
+        data2 = x.finalize(nodata_method='float')
+        assert np.all(data2 == data1)
+
+        # import kwplot
+        # kwplot.autompl()
+        # kwplot.plt.ion()
+        # kwplot.imshow(data1, pnum=(1, 2, 1))
+        # kwplot.imshow(data2, pnum=(1, 2, 2))
+
+
+def off_by_one_with_small_img():
+    """
+    This doesn't work right because of the align corners issue
+    """
+    import delayed_image
+    import kwimage
+    import numpy as np
+    raw = np.linspace(0, 1, 36).reshape(6, 6)
+    delayed = delayed_image.DelayedIdentity(raw)
+
+    warp = kwimage.Affine.coerce(offset=(-1e-8, -1e-9), scale=(8.6, 8.5))
+    x = delayed.warp(warp)
+    x.finalize(interpolation='nearest')
+
+    warp = kwimage.Affine.coerce(offset=(0, 0), scale=(8.6, 8.5))
+    x = delayed.warp(warp)
+    data = x.finalize(interpolation='nearest')
+
+    warp = kwimage.Affine.coerce(offset=(0, 0), scale=(2, 2))
+    warp = warp @ kwimage.Affine.translate((0.5, 0.5))
+    x = delayed.warp(warp)
+    data2 = x.finalize(interpolation='nearest')
+
+    data3 = kwimage.imresize(raw, scale=2.0, interpolation='nearest')
+
+    if 1:
+        import kwplot
+        pnum_ = kwplot.PlotNums(nRows=1, nCols=4)
+        kwplot.autompl()
+        kwplot.plt.ion()
+        kwplot.imshow(raw, pnum=pnum_())
+        kwplot.imshow(kwimage.fill_nans_with_checkers(data), pnum=pnum_())
+        kwplot.imshow(kwimage.fill_nans_with_checkers(data2), pnum=pnum_())
+        kwplot.imshow(kwimage.fill_nans_with_checkers(data3), pnum=pnum_())
