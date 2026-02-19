5a6b55a36f2464a37f831f462605c1d29963010a
Jon Crall <erotemic@gmail.com>
2026-02-18 16:02:24 -0800

    Document antialiasing issue and force inverse warp mapping

--- delayed_image/delayed_nodes.py
+++ delayed_image/delayed_nodes.py
@@ -8,6 +8,7 @@ import copy
 import numpy as np
 import ubelt as ub
 import warnings
+from functools import lru_cache
 from delayed_image import delayed_base
 from delayed_image import delayed_leafs
 from delayed_image.channel_spec import FusedChannelSpec
@@ -23,6 +24,13 @@ from delayed_image.delayed_base import DelayedOperation
 
 TRACE_OPTIMIZE = 0  # TODO: make this a local setting
 IS_DEVELOPING = 0  # set to 1 if hacking in IPython, otherwise 0 for efficiency
+
+
+@lru_cache(maxsize=1)
+def _warp_affine_matrix_mode():
+    """Compatibility switch for kwimage warp matrix convention."""
+    return 'inverse'
+
 
 
 class DelayedArray(delayed_base.DelayedUnaryOperation):
@@ -1617,10 +1625,12 @@ class DelayedWarp(DelayedImage):
         from delayed_image.helpers import _ensure_valid_dsize
         dsize = _ensure_valid_dsize(dsize)
 
-        # kwimage.warp_affine expects a mapping from output-space to
-        # input-space, whereas this node stores the opposite convention.
-        # Convert here to preserve delayed-image transform semantics.
-        M = np.asarray(transform.inv())
+        # kwimage changed matrix convention across versions / backends. Detect
+        # behavior once and choose a compatible matrix mapping.
+        if _warp_affine_matrix_mode() == 'inverse':
+            M = np.asarray(transform.inv())
+        else:
+            M = np.asarray(transform)
 
         # Determine antialiasing from the forward transform semantics.
         # (Passing the inverse transform directly would invert this heuristic.)
